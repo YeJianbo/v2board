1229c465b941d0c20ecad30f18d42e4cfd9d24ac
YeJianbo <2662774551@qq.com>
2026-06-24 08:02:05 +0800

    Update admin traffic overview and node status UI

--- public/assets/admin/assets/NotFoundPage-Di3wpYzA.js
+++
@@ -1,1 +0,0 @@
-import{Ra as e,Va as t,_a as n,ma as r}from"./vendor-DptLH-vH.js";import{b as i}from"./index-Dt5C2fhW.js";var a={class:`not-found`},o=i({__name:`NotFoundPage`,setup(i){return e(()=>{document.title=`404 Not Found`}),(e,i)=>(t(),n(`div`,a,[...i[0]||(i[0]=[r(`h1`,null,`404 Not Found`,-1),r(`p`,null,`nginx`,-1)])]))}},[[`__scopeId`,`data-v-fb2e0a68`]]);export{o as default};
